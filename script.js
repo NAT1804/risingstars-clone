737a505826fc8cc37f93731ed54680371bffe094
Nguyễn Anh Tuấn <anhtuan.jvb@gmail.com>
2022-05-05 17:17:13 +0700

    Add footer

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
 const scrollTrigger = (selector, options = {}) => {
-  let elements = document.querySelectorAll(selector);
+  let elements = document.querySelectorAll(selector)
   Array.from(elements).forEach((element) => {
-    addObserver(element, options);
-  });
-};
+    addObserver(element, options)
+  })
+}
 
 const addObserver = (element, options) => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      entry.target.classList.toggle("add-animation", entry.isIntersecting);
+      entry.target.classList.toggle('add-animation', entry.isIntersecting)
       if (entry.isIntersecting) {
-        observer.unobserve(entry.target);
+        observer.unobserve(entry.target)
       }
-    });
-  }, options);
+    })
+  }, options)
 
-  observer.observe(element);
-};
+  observer.observe(element)
+}
 
-scrollTrigger(".festival-card", { threshold: 0.5 });
-scrollTrigger(".partner-logo", { threshold: 0 });
+scrollTrigger('.festival-card', { threshold: 0.5 })
+scrollTrigger('.partner-logo', { threshold: 0 })
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
